dadaaae24a936042ff7f8c6aaded217125d48f41
Billy Dotson <billybad5@gmail.com>
2022-10-14 16:04:37 -0400

    Add project files.

---
+++ classes and objects/Program.cs
@@ -0,0 +1,10 @@
+﻿using classes_and_objects;
+
+Console.WriteLine("Classes and Objects");
+
+Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+Book book2 = new Book("Lord of the Rings", "Tolkein", 700);
+Book book3 = new Book();
+book2.title = "The Hobbit";
+
+Console.WriteLine(book2.title);
